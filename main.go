7aba310066903c12863a7a8b0c04c0e1a4215b03
rezaafaisal <rezha.faizal567@gmail.com>
2025-01-18 13:03:51 +0800

    tes

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func index(w http.ResponseWriter, r *http.Request) {
 
 func main() {
     http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintln(w, "halo! wkwkwk from test doang lagii, yeaa hahah wkwkw")
+        fmt.Fprintln(w, "halo! wkwkwk from test doang lagii, yeaa hahah wkwkw udah update ajaaa")
     })
 
     http.HandleFunc("/index", index)
